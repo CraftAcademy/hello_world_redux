8eb6b338d30c9a672ccd6b826bbb901598d40cfc
oliverochman <oliver.ochman@hotmail.com>
2020-06-22 10:11:03 +0200

    connect app to redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
-import App from './App';
+import { Provider } from 'react-redux'
+import configureStore from './state/store/configureStore';
+import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+window.store = store;
+
+ReactDOM.render(
+  <Provider store={store}>
+      <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 serviceWorker.unregister();
