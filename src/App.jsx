d6cf1e7eceb1f4c300f7418941b1ba02a046d0d3
oliverochman <oliver.ochman@hotmail.com>
2020-06-22 10:15:35 +0200

    connect app component to redux

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Container, Header } from 'semantic-ui-react'
+import { connect } from 'react-redux';
+
+const App = (props) => {
+  return (
+    <>
+      <Container>
+        <Header as='h1'>{props.state.greeting}</Header>
+      </Container>
+    </>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    state: state
+  }
+}
+
+export default connect(mapStateToProps)(App);
