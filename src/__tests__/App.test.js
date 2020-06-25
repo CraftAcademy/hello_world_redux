fe4c5a9943884761370a9eec8c8901a72542f737
Thomas <thomas@craftacademy.se>
2020-06-25 10:54:56 +0200

    attempst to set a spy on dispatch

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,7 +4,7 @@ import App from '../App'
 import configureStore from 'redux-mock-store';
 import * as redux from 'react-redux'
 describe('<App />', () => {
-  let store, subject
+  let store, subject, dispatch
   const mockStore = configureStore([])
 
   beforeEach(() => {
@@ -12,8 +12,9 @@ describe('<App />', () => {
       greeting: 'Hello World from Redux',
       proposed_greeting: ''
     })
+
     jest.spyOn(redux, 'useSelector').mockReturnValue(store.getState())
-    jest.spyOn(redux, 'useDispatch').mockReturnValue(store.dispatch)
+    dispatch = jest.spyOn(redux, 'useDispatch').mockReturnValue(store.dispatch)
     subject = mount(<App />)
   })
   it('displays the initial greeting set by redux state', () => {
@@ -24,5 +25,20 @@ describe('<App />', () => {
   it('uses <Header as="h1"/>', () => {
     expect(subject.find('Header').prop('as'))
       .toEqual('h1')
+  });
+
+  it('can set PROPOSED_GREETING', () => {
+
+    const input = subject.find('Input');
+    const button = subject.find('button')
+
+    subject.find('input').simulate('change', { target: { value: 'Hello' } })
+    subject.find('input').simulate('blur')
+    // console.log(dispatch.toString())
+    let mockDispatch = jest.fn(dispatch)
+    expect(mockDispatch).toHaveBeenCalledWith(
+      { type: 'PROPOSE_GREETING', greeting: 'Hello' }
+    );
+
   });
 })
