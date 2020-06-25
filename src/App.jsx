fde16a8c6872495081e8d707b37a3624fb29863a
Thomas <thomas@craftacademy.se>
2020-06-25 05:57:43 +0200

    refactors app js to use redux hooks adds solution to test component that uses redux useSelector and useDispatch hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Input, Button, Container, Header } from 'semantic-ui-react'
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
-const App = (props) => {
+const App = () => {
+  const dispatch = useDispatch()
+  const state = useSelector(state => state)
   return (
     <Container>
-      <Header as='h1'>{props.state.greeting}</Header>
+      <Header as='h1'>{state.greeting}</Header>
       <Input
         placeholder='New greeting...'
-        onBlur={(event) => props.dispatch({ type: 'PROPOSE_GREETING', greeting: event.target.value })}
+        onBlur={(event) => dispatch({ type: 'PROPOSE_GREETING', greeting: event.target.value })}
       />
       <Button
         primary
-        onClick={() => props.dispatch({ type: 'CHANGE_GREETING' })}
+        onClick={() => dispatch({ type: 'CHANGE_GREETING' })}
       >
         Change greeting
         </Button>
@@ -20,10 +22,6 @@ const App = (props) => {
   );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    state: state
-  }
-}
 
-export default connect(mapStateToProps)(App);
+
+export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,24 +2,27 @@ import React from 'react'
 import { shallow, mount } from 'enzyme'
 import App from '../App'
 import configureStore from 'redux-mock-store';
-import { Provider } from 'react-redux'
-
-const mockStore = configureStore([])
-
+import * as redux from 'react-redux'
 describe('<App />', () => {
-  let store
+  let store, subject
+  const mockStore = configureStore([])
+
   beforeEach(() => {
     store = mockStore({
       greeting: 'Hello World from Redux',
       proposed_greeting: ''
     })
+    jest.spyOn(redux, 'useSelector').mockReturnValue(store.getState())
+    jest.spyOn(redux, 'useDispatch').mockReturnValue(store.dispatch)
+    subject = mount(<App />)
   })
-  it('should display the initial greeting set by redux state', () => {
-    const subject = mount(
-      <Provider store={store}>
-        <App />
-      </Provider>
-    )
-    expect(subject.find('h1').text()).toEqual('Hello World from Redux')
+  it('displays the initial greeting set by redux state', () => {
+    expect(subject.find('h1').text())
+      .toContain('Hello World from Redux')
   })
+
+  it('uses <Header as="h1"/>', () => {
+    expect(subject.find('Header').prop('as'))
+      .toEqual('h1')
+  });
 })
