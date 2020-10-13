81a10c93722c62f8f1497252f0c6d6264bd2a522
Thomas <thomas@craftacademy.se>
2020-10-13 11:26:28 +0200

    displays greeting from component state using hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
 
-import React, { Component } from 'react';
-import { Container, Header } from 'semantic-ui-react'
-
-class App extends Component {
-  state = {
-    greeting: "Hello World from component state"
-  }
-  render() {
-    return (
-    <Container>
-      <Header as='h1'>{this.state.greeting}</Header>
-    </Container>
-    );
-  }
-}
-
-App.propTypes = {
-
-};
-
-export default App;
-
-// import React from 'react';
+// import React, { Component } from 'react';
 // import { Container, Header } from 'semantic-ui-react'
 
-// const App = () => {
-//   return (
+// class App extends Component {
+//   state = {
+//     greeting: "Hello World from component state"
+//   }
+//   render() {
+//     return (
 //     <Container>
-//       <Header as='h1'>Hello World</Header>
+//       <Header as='h1'>{this.state}</Header>
 //     </Container>
-//   );
+//     );
+//   }
 // }
 
 // export default App;
+
+import React, { useState } from 'react';
+import { Container, Header } from 'semantic-ui-react'
+
+const App = () => {
+  let [greeting, setGreeting] = useState('Hello world from a hooked state')
+  return (
+    <Container>
+      <Header as='h1'>{greeting}</Header>
+    </Container>
+  );
+}
+
+export default App;
