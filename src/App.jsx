c40916da10de436802fc3d5c147e81b8ffd59104
Thomas <thomas@craftacademy.se>
2020-10-13 11:23:46 +0200

    displays greeting from component state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+
+import React, { Component } from 'react';
 import { Container, Header } from 'semantic-ui-react'
 
-const App = () => {
-  return (
+class App extends Component {
+  state = {
+    greeting: "Hello World from component state"
+  }
+  render() {
+    return (
     <Container>
-      <Header as='h1'>Hello World</Header>
+      <Header as='h1'>{this.state.greeting}</Header>
     </Container>
-  );
+    );
+  }
 }
 
+App.propTypes = {
+
+};
+
 export default App;
+
+// import React from 'react';
+// import { Container, Header } from 'semantic-ui-react'
+
+// const App = () => {
+//   return (
+//     <Container>
+//       <Header as='h1'>Hello World</Header>
+//     </Container>
+//   );
+// }
+
+// export default App;
