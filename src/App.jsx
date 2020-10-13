e55ece0449a2586c81746ab67df21211c4614ba9
Thomas <thomas@craftacademy.se>
2020-10-13 11:42:49 +0200

    adds and configures redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,30 @@
 
-// import React, { Component } from 'react';
-// import { Container, Header } from 'semantic-ui-react'
-
-// class App extends Component {
-//   state = {
-//     greeting: "Hello World from component state"
-//   }
-//   render() {
-//     return (
-//     <Container>
-//       <Header as='h1'>{this.state}</Header>
-//     </Container>
-//     );
-//   }
-// }
-
-// export default App;
-
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import { Container, Header } from 'semantic-ui-react'
 
-const App = () => {
-  let [greeting, setGreeting] = useState('Hello world from a hooked state')
-  return (
+class App extends Component {
+
+  render() {
+    return (
     <Container>
-      <Header as='h1'>{greeting}</Header>
+      <Header as='h1'></Header>
     </Container>
-  );
+    );
+  }
 }
 
 export default App;
+
+// import React, { useState } from 'react';
+// import { Container, Header } from 'semantic-ui-react'
+
+// const App = () => {
+//   let [greeting, setGreeting] = useState('Hello world from a hooked state')
+//   return (
+//     <Container>
+//       <Header as='h1'>{greeting}</Header>
+//     </Container>
+//   );
+// }
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import configureStore from './state/store/configureStore'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore()
+window.store = store
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
 serviceWorker.unregister();
