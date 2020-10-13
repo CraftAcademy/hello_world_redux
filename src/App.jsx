e3cc53a5d778dea8d2b9ea20b0fc305b9a1f45d9
Thomas <thomas@craftacademy.se>
2020-10-13 11:55:03 +0200

    makes use of useSelector to get access to store in a functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,39 @@
 
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import { Container, Header } from 'semantic-ui-react'
-
-class App extends Component {
-
-  render() {
-    return (
-      <Container>
-        <Header as='h1'>{this.props.greeting}</Header>
-      </Container>
-    );
-  }
-}
-
-const mapStateToProps = state => {
-  return {
-    greeting: state.greeting
-  }
-}
-
-export default connect(mapStateToProps)(App);
-
-// import React, { useState } from 'react';
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux'
 // import { Container, Header } from 'semantic-ui-react'
 
-// const App = () => {
-//   let [greeting, setGreeting] = useState('Hello world from a hooked state')
-//   return (
-//     <Container>
-//       <Header as='h1'>{greeting}</Header>
-//     </Container>
-//   );
+// class App extends Component {
+
+//   render() {
+//     return (
+//       <Container>
+//         <Header as='h1'>{this.props.greeting}</Header>
+//       </Container>
+//     );
+//   }
 // }
 
-// export default App;
+// const mapStateToProps = state => {
+//   return {
+//     greeting: state.greeting
+//   }
+// }
+
+// export default connect(mapStateToProps)(App);
+
+import React, { useState } from 'react';
+import { Container, Header } from 'semantic-ui-react'
+import { useSelector } from 'react-redux'
+
+const App = () => {
+  // let [greeting, setGreeting] = useState('Hello world from a hooked state')
+  let state = useSelector(state => state)
+  return (
+    <Container>
+      <Header as='h1'>{state.greeting}</Header>
+    </Container>
+  );
+}
+
+export default App;
